a3c014e096a6cfd96e3469dca69848f0f6b0eed4
leo <leo@leodeMacBook-Air.local>
2014-10-01 17:59:20 +0800

    first

---
+++ gitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by leo on 14-10-1.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ gitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by leo on 14-10-1.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
